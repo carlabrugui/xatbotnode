bf38c7bd502ce31e9ba7bdd0deac0c6d3f117a87
carla_brugui <cbrugulat@yahoo.es>
2018-05-15 14:29:48 +0200

    xatbotnode con entrada desconocida

--- cec_connector.js
+++ cec_connector.js
@@ -7,71 +7,81 @@ const request = require('request');
 const urllib = require('urllib');
 
 const invokeFunction = (elementToSearch) => new Promise((resolve, reject) => {
-urllib.request(`https://xatbot-node-cec.herokuapp.com/cecAdmin/search`,
-{ 
-method: 'POST',
-auth: "oracle:welcome1",
-headers: {
-"Content-Type": "application/json",
-"Accept": "application/json"
-},
-data: {
-"type": "EntradaGlosario",
-"name": elementToSearch 
-}
-}, function (err, data, res) {
-if (err) {
-reject(err);
-console.log("ERROR---->: ", err);
-} else {
-var buffer = Buffer.from(new Uint8Array(data));
+    urllib.request(`http://localhost:8080/cecAdmin/search`,
+    { 
+        method: 'POST',
+        auth: "oracle:welcome1",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        data: {
+            "type": "EntradaGlosario",
+            "name": elementToSearch 
+        }
+        }, function (err, data, res) {
+            if (err) {
+                reject(err);
+                console.log("ERROR---->: ", err);
 
-var resp = JSON.parse(buffer.toString());
+            } else {
+                var buffer = Buffer.from(new Uint8Array(data));
 
-var id_CEC = '';
-if (resp.body.items[0].id) {
-id_CEC = resp.body.items[0].id;
-} else {
-id_CEC = '';
-}
-console.log("id_CEC------------------->", id_CEC);
-var url = `https://xatbot-node-cec.herokuapp.com/cecAdmin/wp/${id_CEC}`;
+                var resp = JSON.parse(buffer.toString());
 
-datos_lookup(url)
-.then(data => {resolve(data)})
-.catch(err => console.log("ERROR: ", err));
+                var id_CEC = resp.body.items[0];
 
-}
-});
-});
+                if(id_CEC===undefined){
+                    id_CEC="";
+                }
+                                  
+                id_CEC = id_CEC.id;
+                console.log("id_CEC------------------->", id_CEC);
+                var url = `http://localhost:8080/cecAdmin/wp/${id_CEC}`;
+
+                datos_lookup(url)
+                    .then(data => {resolve(data)})
+                    .catch(err => console.log("ERROR: ", err));
+                
+            }
+        });
+    }
+);
+
 
 const datos_lookup = (url) => new Promise((resolve, reject) => {
-urllib.request(url,
-{
-method: 'GET',
-headers: {
-'Content-Type': 'application/json',
-'Accept': 'application/json'
-},
-}, function (err, data) {
-if (err) {
-reject(err);
-console.log("ERROR datos_lookup: ", err);
-} else {
-var respuesta = JSON.parse(data);
+    urllib.request(url,
+    {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        }, function (err, data) {
+            if (err) {
+                reject(err);
+                console.log("ERROR datos_lookup: ", err);
+            } else {
+                var respuesta = JSON.parse(data);
 
-//var entrada_name = (respuesta.body.data.entradaglosario_nombreentrada).toString();
-var entrada_descr = (respuesta.body.data.entradaglosario_textoentrada).toString();
-;
-//var entrada_photo = (respuesta.body.data.entradaglosario_fotoentrada);
-console.log(entrada_descr);
-//exportar datos para ser usados en la respuesta a DialogFlow
-resolve(entrada_descr); 
-}
-});
+                if(respuesta.body.data ===undefined){
+                    respuesta="Esta palabra no se encuentra en nuestro diccionario";
+                } else {
+                    var entrada_name = (respuesta.body.data.entradaglosario_nombreentrada).toString();
+                    var entrada_descr = (respuesta.body.data.entradaglosario_textoentrada).toString();
 
-});
+                    //exportar datos para ser usados en la respuesta a DialogFlow
+                    respuesta = "Me has preguntado sobre " + entrada_name + ", esta es la infromación que tenemos: " + entrada_descr;
+                }
+
+
+                
+                resolve(respuesta); 
+            }
+        });
+    }
+);
 
 module.exports = {
-invokeFunction
+    invokeFunction
 }; 
--- index.js
+++ index.js
@@ -8,33 +8,43 @@ const http = require('http');
 const cec_connection = require('./cec_connector');
 
 const server = express();
+
 server.use(bodyParser.urlencoded({
-extended: true
+    extended: true
 }));
 
 server.use(bodyParser.json());
 
 server.post('/conexionCEC', (req, res) => {
 
-const elementToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.Entradas ? req.body.result.parameters.Entradas : 'zonulina';
-let msg = 'Me has preguntado sobre: ' + elementToSearch;
+let elementToSearch = req.body.result.parameters.Entradas;
+
+if (elementToSearch===""){
+    elementToSearch = req.body.result.resolvedQuery;
+
+    elementToSearch = elementToSearch.split(" ");
+    const sizeSearch = elementToSearch.length;
+    elementToSearch = elementToSearch[sizeSearch-1];
+
+    elementToSearch = elementToSearch.split("?");
+    elementToSearch = elementToSearch[0];
+};
+
 
 //Invocar cec-restapi
 //Obtener content item
 //Variable para recuperar content item y printar al xatbot
 const item = cec_connection.invokeFunction(elementToSearch)
-.then(data => {
-res.json({
-speech: data,
-displayText: data,
-source: 'get-respuesta'
-});
-})
-.catch(err => console.log("ERROR: ", err));
-//const item = cec_connection.recuperacion;
-/**/
+    .then(data => {
+        res.json({
+        speech: data,
+        displayText: data,
+        source: 'get-respuesta'
+        });
+    })
+    .catch(err => console.log("ERROR: ", err));
 });
 
 server.listen((process.env.PORT || 8000), () => {
-console.log("Server is up and running...");
+    console.log("Server is up and running...");
 }); 
